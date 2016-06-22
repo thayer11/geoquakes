e8627b49c850367275821914d5b5f9c257afe9cc
thayer11 <holly.coddington@gmail.com>
2016-06-22 15:40:26 -0600

    data-list-created

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,20 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
-var template;
+
+
+var $results = $("#info");
+var source = $("#earthquakes-list").html();
+var template = Handlebars.compile(source);
 
 $(document).on("ready", function() {
 
+	$.get(weekly_quakes_endpoint, function (data) {  
+
+	    console.log(data);
+	    var trackHTML = template({ earthquakes: data.features });
+	    $results.append(trackHTML);
+
+	});	
 
 });
