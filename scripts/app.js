48cf550041968aeece4f4b31e3b54e08692f6570
thayer11 <holly.coddington@gmail.com>
2016-06-22 18:36:43 -0600

    dropped-pins-iteration

--- scripts/app.js
+++ scripts/app.js
@@ -8,14 +8,42 @@ var $results = $("#info");
 var source = $("#earthquakes-list").html();
 var template = Handlebars.compile(source);
 
-$(document).on("ready", function() {
+
+	myMap();
 
 	$.get(weekly_quakes_endpoint, function (data) {  
 
 	    console.log(data);
 	    var trackHTML = template({ earthquakes: data.features });
 	    $results.append(trackHTML);
+	    var listQuakes = data.features;
+	    console.log(listQuakes);
 
-	});	
+	    for (var i=0; i<listQuakes.length; i++) {
+	    	var latitude=listQuakes[i].geometry.coordinates[0];
+	    	var longitude=listQuakes[i].geometry.coordinates[1];
+	    
+			var myLatLng = {lat: latitude, lng: longitude};
 
-});
+			var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+	});
+	    }
+
+	});
+
+
+	function myMap() {
+		var myLatLng = {lat: 39.76, lng: -105.01};
+
+
+		map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 2, 
+		center: myLatLng
+	});
+	
+}
+
+	
+
